9d5b53ff7339e7beb9bc63cd2fd5b562fa2b9479
MarynaPyvovar <marina18renkas@gmail.com>
2022-07-19 19:26:59 +0300

    01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,22 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+const imagesSet = createImagesMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', imagesSet);
+
+function createImagesMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {});
