d67aa809a23644a4dd2902ebd206e277c1ccf003
MarynaPyvovar <marina18renkas@gmail.com>
2022-07-21 20:50:11 +0300

    feat 02-video + some 03-feedback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', () => {
+  throttle(
+    player
+      .getCurrentTime()
+      .then(time => localStorage.setItem('videoplayer-current-time', time)),
+    1000
+  );
+});
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit());
+refs.email.addEventListener('input'.onInputChange());
+refs.message.addEventListener('input'.onInputChange());
+
+function onFormSubmit() {}
+
+function onInputChange(e) {
+  const text = e.target.value;
+  localStorage.setItem('message', text);
+}
